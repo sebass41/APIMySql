7750c963d22fbb19f024889f6f4cf4499869fc31
Sebastian <ssdible@gmail.com>
2024-08-13 20:58:06 -0300

    Errores .1

--- backend/model/ProductoDAO.php
+++ backend/model/ProductoDAO.php
@@ -1,15 +1,50 @@
 <?php
 
 require_once "../Conexion.php";
+require_once "res/Respuesta.php";
+
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+
+ini_set('log_errors', 1);
+ini_set('error_log', '../log/php_errors.log');
 
 class Producto{
 
     function insertar($id, $title, $permalink, $thumbnail, $price){
-        $sql = "INSERT INTO productos VALUES ('$id', '$title', '$permalink', '$thumbnail', $price)";
+        try{
+            $conection = conection();
+            $sql = "INSERT INTO productos VALUES ('$id', '$title', '$permalink', '$thumbnail', $price)";
+            $respuesta = $conection->query($sql);
+
+            if ($respuesta){
+                $msj = "Se pudo insertar correctamente";
+                return new Respuesta(true, $msj, $respuesta);
+            }else{
+                throw new Exception("Error al insertar los datos");
+            }
+        } catch (Exception $e){
+            $msj = $e->getMessage();
+            return new Respuesta(false, $msj, []);
+        }
+        
+        
+        
+    }
+
+    function actualizar($id, $title, $permalink, $thumbnail, $price){
+        $sql = "UPDATE productos SET Title = '$title', Permalink = '$permalink', Thumbnail = '$thumbnail', Price = $price WHERE id = '$id'";
         $conection = conection();
         $respuesta = $conection->query($sql);
-        return $respuesta;
+        
+        if ($respuesta){
+            $msj = "Se modificó correctamente";
+            return new Respuesta(true, $msj, $respuesta);
+        }else{
+            $msj = "Error al modificar los datos";
+            return new Respuesta(false, $msj, $respuesta);
+        }
     }
-}
 
+}
 ?>
